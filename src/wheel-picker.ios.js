f2e43d2d774fc49bf0e89b71588536e05d64511b
hossein es <nightmare.hossein@gmail.com>
2019-09-05 11:52:19 +0430

    Fix Ios Bug

--- src/wheel-picker.ios.js
+++ src/wheel-picker.ios.js
@@ -12,43 +12,44 @@ type State = {
 }
 
 export default class WheelPicker extends React.Component<Props, State> {
-  static defaultProps = {
-    style: {
-      width: 200,
-      height: 150,
-    },
-  }
-
-  constructor(props: Props){
-    super(props)
-    this.state = {
-      selectedItem: props.selectedItem
+    static defaultProps = {
+        style: {
+            width: '90%', 
+            height: '100%'
+        },
     }
-  }
-  
-  componentDidUpdate(prevProps: Props, prevState: State){
-    if (prevState.selectedItem !== this.props.selectedItem){
-      this.setState({ selectedItem: this.props.selectedItem })
-    }
-  }
 
-  onItemSelected = (value: any, index: number) => {
-    if (this.props.onItemSelected) {
-      this.props.onItemSelected(index)
+    constructor(props: Props){
+        super(props)
+        this.state = {
+        selectedItem: props.selectedItem
+        }
     }
-    this.setState({selectedItem: index})
-  }
+    
+    componentDidUpdate(prevProps: Props, prevState: State){
+        if (prevState.selectedItem !== this.props.selectedItem){
+        this.setState({ selectedItem: this.props.selectedItem })
+        }
+    }
 
-  render() {
-    const data = this.props.data
-    if (!data || !data.length < 0) return null
-    return (
-      <Picker
-      {...this.props}
-      selectedValue={data[this.state.selectedItem]}
-      onValueChange={this.onItemSelected}>
-      {this.props.data.map((i, index) => <Picker.Item key={index} label={i} value={i} />)}
-    </Picker>
-    )
-  }
+    onItemSelected = (value: any, index: number) => {
+        if (this.props.onItemSelected) {
+        this.props.onItemSelected(index)
+        }
+        this.setState({selectedItem: index})
+    }
+
+    render() {
+        const data = this.props.data
+        if (!data || !data.length < 0) return null
+        return (
+            <Picker
+                {...this.props}
+                itemStyle = {{ width: '90%', height: '100%'}}
+                selectedValue={data[this.state.selectedItem]}
+                onValueChange={this.onItemSelected}>
+                {this.props.data.map((i, index) => <Picker.Item key={index} label={i} value={i} />)}
+            </Picker>
+        )
+    }
 } 
